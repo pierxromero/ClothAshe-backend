2487885e134079ef75d1a70138c90520cc4c2069
Pierx <pierofelixvisitacionromero@gmail.com>
2025-05-24 09:21:01 -0300

    First commit

---
+++ src/main/java/com/clothashe/clotashe_backend/ClotasheBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clothashe.clotashe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClotasheBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClotasheBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clothashe/clotashe_backend/ClotasheBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothashe.clotashe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClotasheBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
